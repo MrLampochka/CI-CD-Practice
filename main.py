f6b48d1b97812c680d91a7f3173b1d3f95ac0911
Valentin Khakimov <mr.lampochka.ru@gmail.com>
2025-09-10 18:34:28 +0300

    feat: lol invite

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import requests
 
 load_dotenv(find_dotenv())
 
-TOKEN = os.getenv("TOKEN")      # замените на ваш токен
-CHAT_ID = os.getenv("CHAT_ID")                  # замените на chat_id (int или строка "@channelusername")
-TEXT = "Привет от бота!"
+TOKEN = os.getenv("TOKEN")  # замените на ваш токен
+CHAT_ID = os.getenv("CHAT_ID")  # замените на chat_id (int или строка "@channelusername")
+
+TEXT = "Олег привет от ci cd бота и погнали в лол"
+
 # Request messages
 # url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 
